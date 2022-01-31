5163b5fd45f135b847a765f45d85fda49587abb2
Willian Komada <willian10komada@hotmail.com>
2022-01-31 11:14:14 -0300

    adicionando uma lista ordenada

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ export default function App() {
   return (
     <div>
       <h1>How to deploy app in Netlify.</h1>
+
+      <ol>
+        <li>Publicar um app criado com create react-app;</li>
+        <li>
+          Criar um app - yarn create react-app nameOfProject --template
+          typescript;
+        </li>
+        <li>Entrar no site do Netlify;</li>
+        <li>Acessar a conta;</li>
+        <li>Clicar em Add new site - Importing an existing project;</li>
+        <li>Selecionar o projeto e clicar em deploy.</li>
+      </ol>
     </div>
   );
 }
